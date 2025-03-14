9c3835fa8797e830f660b52e3296436d93e4f3ed
Tim Horne <timhornedrums@gmail.com>
2025-03-14 13:01:28 +0000

    Version 1

---
+++ CodeCademy.js
@@ -0,0 +1,41 @@
+var number = 5; // in-line comment
+
+/* this is a
+multi-line comment */
+
+/* In computer science data is anything that is 
+meaningful to a computer./*
+
+/* Data Types:
+undefined - undefined data eg. an undefined variable
+null - nothing, it has been set to something and
+that something is nothing.
+boolean - true of false data type
+string - any sort of text 
+symbol - an imutable primitaive value 
+object - stores multiple key value pairs */
+
+/* variables enable us to store and manipulate data in
+a dynamic fashion. Its a closet to put data inside.
+It has a label which is the variable name. When
+that name is used it points to the data stored 
+inside */
+
+var myName = "Tim" /* A variable which can be used 
+throughout the entire code. */
+
+myName = 9 
+
+let ourName = "freeCodeCamp" /* Can only be used
+within the scope of where it is declared */
+
+const pi = 3.14 // A variable that can never change
+
+var a; // This is declaring a variable
+var b = 2; // Here we are declaring and assigning a
+// variable. The "=" is the assignment operator.
+
+a = "I love VS Code"; /* Here, a is being assigned to a vlaue.
+we done have to declare it because its been declared.*/
+
+console.log(a) // This will print "I love VS Code"
